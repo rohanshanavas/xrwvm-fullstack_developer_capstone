3c01dd6ae474ad5b5ab943840a2cfdea4a320368
Rohan Shanavas <34649755+rohanshanavas@users.noreply.github.com>
2024-09-05 12:40:08 +0100

    Fixed linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,15 +19,15 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
 
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    CAR_TYPES = [('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'
-                 ), ('SPORT', 'Sport')]
+    CAR_TYPES = [('SEDAN', 'Sedan'), ('SUV', 'SUV'), 
+                 ('WAGON', 'Wagon'), ('SPORT', 'Sport')]
     type = models.CharField(max_length=10, choices=CAR_TYPES,
                             default='SUV')
     year = models.IntegerField(default=2023,
                                validators=[MaxValueValidator(2023),
-                               MinValueValidator(2015)])
+                                           MinValueValidator(2015)])
 
     # Other fields as needed
 
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://rohanshanava-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://rohanshanava-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost',
+                 'https://rohanshanava-8000.theiadockernext-0-labs-prod' +
+                 '-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://rohanshanava-8000.theiadockernext-0-labs' +
+                        '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,7 +65,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
@@ -94,7 +97,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -139,8 +143,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
